68384d1fae455d5f15557bcbe71767a3f0a80e06
jaikangam <jaikangam12@gmail.com>
2022-06-12 01:32:20 +0530

    Added navbar contact page

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+
+
+const contact = () => {
+  return (
+    <div className="text-center mt-20">
+    
+      <h1 className="text-4xl font-bold">Contact US</h1>
+    </div>
+  );
+};
+
+export default contact;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <div >
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+      <Hero />
     </div>
   )
 }
